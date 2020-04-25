22f7bb2ba91ead567c9b05fe204ca6293a5d9940
wuweifu <minidoras15@gamil.com>
2020-04-25 19:53:31 +0800

    步骤一 · 2-23 使用PostMan调试Restful接口

--- foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
@@ -1,8 +1,10 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Stu;
 import com.imooc.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,6 +22,24 @@ public class StuFooController {
     @GetMapping("/getStu")
     public Object getStu(int id) {
         return stuService.getStuInfo(id);
+    }
+
+    @PostMapping("/save")
+    public Object saveStu() {
+        stuService.saveStu();
+        return "OK";
+    }
+
+    @PostMapping("/updateStu")
+    public Object updateStu(int id) {
+        stuService.updateStu(id);
+        return "OK";
+    }
+
+    @PostMapping("/deleteStu")
+    public Object deleteStu(int id) {
+        stuService.deleteStu(id);
+        return "OK";
     }
 
 }
--- foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/StuServiceImpl.java
@@ -21,24 +21,34 @@ public class StuServiceImpl implements StuService {
     @Autowired
     private StuMapper stuMapper;
 
-    @Transactional(propagation = Propagation.REQUIRED)
+    @Transactional(propagation = Propagation.SUPPORTS)
     @Override
     public Stu getStuInfo(int id) {
         return stuMapper.selectByPrimaryKey(id);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void saveStu() {
-
+        Stu stu = new Stu();
+        stu.setAge(12);
+        stu.setName("wwf");
+        stuMapper.insert(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void updateStu(int id) {
-
+        Stu stu = new Stu();
+        stu.setId(id);
+        stu.setAge(18);
+        stu.setName("vla");
+        stuMapper.updateByPrimaryKey(stu);
     }
 
+    @Transactional(propagation = Propagation.REQUIRED)
     @Override
     public void deleteStu(int id) {
-
+        stuMapper.deleteByPrimaryKey(id);
     }
 }
