6a01ec934c06b8aa039b76aacc2485b878df89b0
wuweifu <minidoras15@gamil.com>
2020-04-26 01:29:06 +0800

    步骤一 · 4-5 用户注册 - 创建用户controller与测试

--- foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,13 +1,13 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.bo.UserBo;
 import com.imooc.service.UserService;
 import com.imooc.utils.IMOOCJSONResult;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
 
 /**
  * @Author wuweifu
@@ -35,6 +35,40 @@ public class PassportController {
         if (isExist) {
             return IMOOCJSONResult.errorMsg("用户名已经存在");
         }
+
+        return IMOOCJSONResult.ok();
+    }
+
+    @PostMapping("/regist")
+    public IMOOCJSONResult regist(@RequestBody UserBo userBo) {
+
+        String username = userBo.getUsername();
+        String password = userBo.getPassword();
+        String confirmPassword = userBo.getConfirmPassword();
+
+        /** 判断用户名和密码非空 */
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password) || StringUtils.isBlank(confirmPassword)) {
+            return IMOOCJSONResult.errorMsg("用户名或密码不能为空");
+        }
+
+        /** 查询用户名是否已经存在 */
+        boolean isExist = userService.queryUsernameIsExist(username);
+        if (isExist) {
+            return IMOOCJSONResult.errorMsg("用户名已经存在");
+        }
+
+        /** 判断密码长度是否大于等于6位 */
+        if (password.length() < 6) {
+            return IMOOCJSONResult.errorMsg("密码长度不能小于6位");
+        }
+
+        /** 判断2次密码是否一致 */
+        if (!password.equals(confirmPassword)) {
+            return IMOOCJSONResult.errorMsg("两次密码输入不一致");
+        }
+
+        /** 注册 */
+        userService.createUser(userBo);
 
         return IMOOCJSONResult.ok();
     }
