f563f46e3e06020e7b0841a03020f587fd87060e
wuweifu <minidoras15@gamil.com>
2020-05-05 02:23:42 +0800

    2-1 2-2 实现接口联调

--- foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.imooc.enums.YesOrNo;
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.Category;
 import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
 import com.imooc.service.CarouselService;
 import com.imooc.service.CategoryService;
 import com.imooc.utils.IMOOCJSONResult;
@@ -67,6 +68,19 @@ public class IndexController {
         List<CategoryVO> subCatList = categoryService.getSubCatList(rootCatId);
 
         return IMOOCJSONResult.ok(subCatList);
+    }
+
+    @ApiOperation(value = "查询每个一级分类下的最新6条商品数据", notes = "查询每个一级分类下的最新6条商品数据", httpMethod = "GET")
+    @GetMapping("/sixNewItems/{rootCatId}")
+    public IMOOCJSONResult sixNewItems(@PathVariable Integer rootCatId) {
+
+        if (null == rootCatId) {
+            return IMOOCJSONResult.errorMsg("分类不存在");
+        }
+
+        List<NewItemsVO> sixNewItemsLazy = categoryService.getSixNewItemsLazy(rootCatId);
+
+        return IMOOCJSONResult.ok(sixNewItemsLazy);
     }
 
 
--- foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import com.imooc.mapper.CategoryMapper;
 import com.imooc.mapper.CategoryMapperCustom;
 import com.imooc.pojo.Category;
 import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.NewItemsVO;
 import com.imooc.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -49,5 +51,15 @@ public class CategoryServiceImpl implements CategoryService {
         List<CategoryVO> subCatList = categoryMapperCustom.getSubCatList(rootCatId);
 
         return subCatList;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<NewItemsVO> getSixNewItemsLazy(Integer rootCatId) {
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("rootCatId", rootCatId);
+
+        return categoryMapperCustom.getSixNewItemsLazy(map);
     }
 }
