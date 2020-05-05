5c4821f90a69d28b612459c273aefd20febd09d8
wuweifu <minidoras15@gamil.com>
2020-05-05 20:17:21 +0800

    2-3 2-4 编写商品相关查询service

---
+++ foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -0,0 +1,46 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
+/**
+ * @Author wuweifu
+ * @Date 2020/5/5 7:03 下午
+ * @Version V1.0
+ * @Description:
+ **/
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @param id
+     * @return
+     */
+    public Items queryItemById(String id);
+
+    /**
+     * 根据商品id查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品ID查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品ID查询商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+}
