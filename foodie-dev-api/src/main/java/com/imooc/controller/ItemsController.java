0ad55c850865dc83c64aa13a609f0411ed0f0b46
wuweifu <minidoras15@gamil.com>
2020-05-05 21:47:34 +0800

    2-5

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,60 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.service.ItemService;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @Author wuweifu
+ * @Date 2020/5/5 8:25 下午
+ * @Version V1.0
+ * @Description: 商品详情
+ **/
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("/items")
+public class ItemsController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "", notes = "", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg("商品id不能为空");
+        }
+
+        Items items = itemService.queryItemById(itemId);
+        List<ItemsImg> itemsImgs = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecs = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(items);
+        itemInfoVO.setItemImgList(itemsImgs);
+        itemInfoVO.setItemSpecList(itemsSpecs);
+        itemInfoVO.setItemParams(itemsParam);
+
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+}
