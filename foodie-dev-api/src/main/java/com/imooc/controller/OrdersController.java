ab681b319da50e7936c5d8763b0042f96644a4c4
wuweifu <minidoras15@gamil.com>
2020-05-14 00:08:48 +0800

    2-1 - 2-4 提交并且接受订单信息

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -0,0 +1,41 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author wuweifu
+ * @Date 2020/5/11 11:19 下午
+ * @Version V1.0
+ * @Description: 用户地址API
+ **/
+@Api(value = "订单", tags = "订单API")
+@RestController
+@RequestMapping("/orders")
+public class OrdersController {
+
+
+
+    @ApiOperation(value = "用户下订单", notes = "用户下订单", httpMethod = "POST")
+    @PostMapping("/create")
+    public IMOOCJSONResult list(@RequestBody SubmitOrderBO submitOrderBO) {
+
+        System.out.println(submitOrderBO);
+
+        /** 1.创建订单 */
+
+        /** 2.创建订单以后 移除购物车中已结算（已提交）的商品 */
+
+        /** 3.向支付中心发送当前订单，用户保存支付中心的订单数据 */
+
+        return IMOOCJSONResult.ok();
+    }
+
+
+}
