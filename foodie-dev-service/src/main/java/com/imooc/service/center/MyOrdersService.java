15b695bdebb42f3f4e9c270911b49cb6aa008d3f
wuweifu <minidoras15@gamil.com>
2020-07-13 23:23:47 +0800

    3-1 订单管理-查询我的订单SQL 3-2 订单管理-编写查询Mapper与Service

---
+++ foodie-dev-service/src/main/java/com/imooc/service/center/MyOrdersService.java
@@ -0,0 +1,28 @@
+package com.imooc.service.center;
+
+
+import com.imooc.utils.PagedGridResult;
+
+/**
+ * @author wuweifu
+ *  用户中心-订单管理接口
+ */
+public interface MyOrdersService {
+
+    /**
+     * 查询我的订单列表
+     * @param userId    用户ID
+     * @param orderStatus   订单状态
+     * @param page  页码
+     * @param pageSize  每页显示条数
+     * @return
+     */
+    public PagedGridResult queryMyOrders(String userId,
+                                         Integer orderStatus,
+                                         Integer page,
+                                         Integer pageSize
+    );
+
+
+
+}
